b6a92af2b2b22ecd59fc18497c3e90d57ab58cea
janezlapajne <lapajne.janez@gmail.com>
2024-08-27 22:48:42 +0200

    refactor: Move image label extraction to separate helpers file

--- source/helpers/__init__.py
+++ source/helpers/__init__.py
@@ -2,6 +2,7 @@ from .artifacts import load_transformation_matrix, save_transformation_matrix
 from .helpers import (
     extract_labels_from_spectral_image,
     extract_labels_from_spectral_images,
+    get_images_by_label,
     read_spectral_images,
 )
 
@@ -11,4 +12,5 @@ __all__ = [
     "extract_labels_from_spectral_images",
     "save_transformation_matrix",
     "load_transformation_matrix",
+    "get_images_by_label",
 ]
--- source/helpers/helpers.py
+++ source/helpers/helpers.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from siapy.entities.imagesets import SpectralImage, SpectralImageSet
 
-from source.core import settings
+from source.core import logger, settings
 
 
 def read_spectral_images() -> tuple[list[SpectralImage], list[SpectralImage]]:
@@ -51,3 +51,35 @@ def extract_labels_from_spectral_images(
     ]
 
     return labels_cam1, labels_cam2
+
+
+def get_images_by_label(
+    label: str,
+    image_set_cam1: list[SpectralImage],
+    image_set_cam2: list[SpectralImage],
+    labels_cam1: list[list[str]],
+    labels_cam2: list[list[str]],
+) -> tuple[SpectralImage, SpectralImage]:
+    index_cam1 = -1
+    index_cam2 = -1
+    for idx, labels_list in enumerate(labels_cam1):
+        if label in labels_list:
+            index_cam1 = idx
+            break
+    for idx, labels_list in enumerate(labels_cam2):
+        if label in labels_list:
+            index_cam2 = idx
+            break
+
+    if index_cam1 == -1:
+        raise ValueError(f"Label for '{settings.camera1_id}' was not found.")
+    if index_cam2 == -1:
+        raise ValueError(f"Label for '{settings.camera2_id}' was not found.")
+
+    image_cam1 = image_set_cam1[index_cam1]
+    image_cam2 = image_set_cam2[index_cam2]
+
+    logger.info(f"Extracted images for label: '{label}'")
+    logger.info(f"Camera1 image path: '{image_cam1.filepath}'")
+    logger.info(f"Camera2 image path: '{image_cam2.filepath}'")
+    return image_cam1, image_cam2
