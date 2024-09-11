9df738030f3a5200088d6c581b02a068fd5d50d2
janezlapajne <lapajne.janez@gmail.com>
2024-09-11 11:10:33 +0200

    fix: check added to reflectance converter to apply correction only if images equal.

--- source/processing/converter.py
+++ source/processing/converter.py
@@ -22,29 +22,42 @@ def calculate_correction_factor_from_panel(
     return panel_correction
 
 
-def convert_images_to_reflectance():
-    image_set_cam1, image_set_cam2 = load_radiance_images()
+def _convert_imageset_to_reflectance(image_set: list[SpectralImage]):
+    panel_correction: None | np.ndarray = None
+    panel_image_idx: None | str = None
 
-    # filename_widx = (
-    #     f"{index}{settings.labels_between_deliminator}"
-    #     f"{idx}{settings.labels_part_deliminator}"
-    #     f"{filename_cam1}.hdr"
-    # )
-    for image in image_set_cam1:
+    for image in image_set:
         filename = image.filepath.stem
         # Here the assumption is made that first object is reference panel
-        object_idx = filename.split(settings.labels_part_deliminator)[0].split(
-            settings.labels_between_deliminator
-        )[1]
+        image_idx, object_idx = filename.split(settings.labels_part_deliminator)[
+            0
+        ].split(settings.labels_between_deliminator)
         if object_idx == "0":
             panel_correction = calculate_correction_factor_from_panel(
                 image=image,
                 panel_reference_reflectance=settings.panel_reflectance,
             )
-            continue
+            panel_image_idx = image_idx
+            # continue # uncomment if reference panel should not be saved
 
-        filepath = f"{filename}{settings.labels_between_deliminator}reflactance.hdr"
-        image_np = convert_radiance_image_to_reflectance(
-            image=image, panel_correction=panel_correction, save_path=None
-        )
-        save_reflectance_image(image_np, filepath, image.metadata)
+        if (
+            panel_correction is not None
+            and panel_image_idx is not None
+            and image_idx == panel_image_idx
+        ):
+            filepath = f"{filename}{settings.labels_between_deliminator}reflactance.hdr"
+            image_np = convert_radiance_image_to_reflectance(
+                image=image, panel_correction=panel_correction, save_path=None
+            )
+            save_reflectance_image(image_np, filepath, image.metadata)
+        else:
+            raise ValueError(
+                "Something went wrong. Internal error. "
+                "If reference panel has idx=0 and the paths are sorted correctly this should not happen."
+            )
+
+
+def convert_images_to_reflectance():
+    image_set_cam1, image_set_cam2 = load_radiance_images()
+    _convert_imageset_to_reflectance(image_set_cam1)
+    _convert_imageset_to_reflectance(image_set_cam2)
